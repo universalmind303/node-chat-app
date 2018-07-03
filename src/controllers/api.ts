0c8985a656dc6810114586dcdc45e442afdc3357
universalmind303 <universalmind.candy@gmail.com>
2018-07-02 21:54:10 -0500

    refactor: add config for vscode debugger to work properly

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 const api = express.Router();
-api.get("/hello", (req, res) => res.send("hellsassssddddo"));
-api.get("/hello1", (req, res) => res.send("hello"));
+api.get("/hello", (req, res) => res.send("hello"));
+api.get("/hello1", (req, res) => res.send("hello1"));
 
 export default api;
